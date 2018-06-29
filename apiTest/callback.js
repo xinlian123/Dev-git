4dc4d72c37c97a72689cc68c4e1030df3436fc90
xinlian123 <lianxin9342@gmail.com>
2018-06-29 14:27:47 -0400

    Test FRapi

---
+++ apiTest/callback.js
@@ -0,0 +1,7 @@
+var config = {
+  username : 'openidm-admin',
+  pwd : 'openidm-admin',
+  server: 'http://tu-mtfridm-01:8080/openidm/system/TestDJ/account/',
+  headerUSR: 'x-openidm-username',
+  headerPWD: 'x-openidm-password'    
+}
